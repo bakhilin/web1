422a7a7a5975ad1870324e01b8b76fcd40ac2527
bakhilin <bahilinnikita04@mail.ru>
2023-09-10 22:47:32 +0300

    dsd

--- pages/guest.php
+++ pages/guest.php
@@ -2,22 +2,18 @@
 <div class="guest-content">
     <div class="gif-block">
         <div class="gifs">
-            <img src="/img/elon-musk-smoke.gif" alt="">
-            <img src="/img/keanu-reeves-matrix.gif" alt="">
-            <img src="/img/tyler-durden.gif" alt="">
-            <img src="/img/fight-club-brad-pitt.gif" alt="" style="height: 150%">
+            
         </div>
     </div>
     <div style="margin-top: 15%" class="links">
-        <ol>
+        <ol style="list-style-type:none">
             <li><a href="index.php?page=2" class="arrow"><i class="fas fa-arrow-alt-right"></i>Лабораторная №1</a></li>
             <li><a href="index.php?page=1" class="arrow"><i class="fas fa-arrow-alt-right"></i>Главная</a></li>
         </ol>
     </div>
     <div class="gif-block">
         <div class="gifs">
-            <img src="/img/the-terminator-arnold-schwarzenegger.gif " alt="">
-            <img src="https://media.tenor.com/OoNXQi4CrW0AAAAd/vandame_cena3.gif" alt="" style="height: 150%">
+            
         </div>
     </div>
 </div>
