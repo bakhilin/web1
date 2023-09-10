59e9709c1cbcd499888d3fd8ffbb9dbfd25aa973
bakhilin <bahilinnikita04@mail.ru>
2023-09-10 23:17:34 +0300

    dsd

--- pages/guest.php
+++ pages/guest.php
@@ -8,7 +8,6 @@
     <div style="margin-top: 15%" class="links">
         <ol style="list-style-type:none">
             <li><a href="index.php?page=2" class="arrow"><i class="fas fa-arrow-alt-right"></i>Лабораторная №1</a></li>
-            <li><a href="index.php?page=1" class="arrow"><i class="fas fa-arrow-alt-right"></i>Главная</a></li>
         </ol>
     </div>
     <div class="gif-block">
--- script.js
+++ script.js
@@ -53,3 +53,5 @@ addEvent(window, "load", function () {
     }
 });
 
+
+
