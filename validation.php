10856b696d3091e9d2aa7f734a3f132335f53c86
Jim Kerry <bahilinnikita04@mail.ru>
2023-09-28 12:11:09 +0300

    123

--- validation.php
+++ validation.php
@@ -3,7 +3,9 @@
 include "functions.php";
 
 
-//var_dump($_REQUEST);
+var_dump($_REQUEST);
+
+//header("Location: index.php?page=1");
 
 if (checkFieldX($_POST['changeR'], $_POST['x']) & checkFieldY($_POST['changeR'], $_POST['fieldY'])) {
     $flag = true;
@@ -13,7 +15,9 @@ if (checkFieldX($_POST['changeR'], $_POST['x']) & checkFieldY($_POST['changeR'],
 
 $microTime = microtime();
 
+
 if ($_POST['x'] >= -5 and $_POST['x'] <= 3) {
+
     if ($_POST['fieldY'] >=-5 and $_POST['fieldY'] <= 5) {
         if ($_POST['changeR'] >= 1 and $_POST['changeR'] <=5) {
             $time = date("H:i:s");
