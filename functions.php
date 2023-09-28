70dff47a93c9c454929810e3c5774d22e5f3d9b7
bakhilin <bahilinnikita04@mail.ru>
2023-09-28 11:56:43 +0300

    12

--- functions.php
+++ functions.php
@@ -27,15 +27,15 @@ function getPage($page): array
 function checkFieldX($r, $x) {
     $radius = (float)$r / 2;
     if($radius >=0) {
-        if($x >=0 ) {
-            if($x <= $radius) {
+        if((float)$x >=0 ) {
+            if((float)$x <= $radius) {
                 return true;
             } else {
                 return false;
             }
         } else {
             $radius = $radius * (-1);
-            if($radius <= $x) {
+            if($radius <= (float)$x) {
                 return true;
             } 
 
@@ -47,14 +47,14 @@ function checkFieldX($r, $x) {
 }
 
 function checkFieldY($r, $y) {
-    if ($y < 0) {
-        if($y >= ($r* (-1))) {
+    if ((float)$y < 0) {
+        if((float)$y >= ((float)$r* (-1))) {
             return true;
         } else {
             return false;
         }
     } else {
-        if($y <= $r) {
+        if((float)$y <= (floatgit )$r) {
             return true;
         } 
         return false;
