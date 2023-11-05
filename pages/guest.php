68538cc8e34664e74f3a66e38b4c46df023615c1
Nikita <bahilinnikita04@mail.ru>
2023-11-05 23:23:55 +0300

    delete all commits

--- pages/guest.php
+++
@@ -1,9 +0,0 @@
-
-<div class="guest-content">
-    <div style="margin-top: 15%" class="links">
-        <ol style="list-style-type:none">
-            <li> <a href="index.php?page=2" class="arrow"><i class="fas fa-arrow-alt-right"></i>Лабораторная №1</a></li>
-        </ol>
-    </div>
-</div>
-
